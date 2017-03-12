6505e2028b17dbff2452d451766a0355f49bf858
Alacr1ty <corderda@oregonstate.edu>
2017-03-11 18:56:41 -0800

    Added description comments, main(), if __name__ statement, and shebang line ~Ava

--- tsp.py
+++ tsp.py
@@ -1,11 +1,26 @@
+#!/usr/bin/env
+'''
+# CS325 Project 4 - Traveling Salesman Problem
 # Ava Cordero
 # Alston Godbolt
 # Soo-Min Yoo
-
-# CS325 Project 4 - Traveling Salesman Problem
-
+	Nearest Insertion TSP : By Soo-Min, Ava, and Alston
+	<# I'm open to using a different alg too ~Ava >
+		Starting from a degenerate tour consisting of the two closest 
+	cities, repeatedly choose the non-tour city with the minimum 
+	distance to its nearest neighbor among the tour cities, and insert 
+	it in between the two consecutive tour cities for which such an 
+	insertion causes the minimum increase in total tour length.
+'''
 
 # TSP Algorithm
 def tsp():
 
 	return 0
+
+def main ():
+	tsp()
+
+
+if __name__ == '__main__':
+	main()
