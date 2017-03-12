99103ccdceb8ed3fdadbf2e39d83dc04004e91aa
Alacr1ty <corderda@oregonstate.edu>
2017-03-11 20:50:28 -0800

    Added in general functions and gave the program i/o, reads a graph from a text file into a dict and passes the graph through each function to print the graph to the console. there is a newline char at the end of each dict entry. This is a first draft.

--- tsp.py
+++ tsp.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env
+
 '''
 # CS325 Project 4 - Traveling Salesman Problem
 # Ava Cordero
@@ -11,13 +12,41 @@ it in between the two consecutive tour cities for which such an
 insertion causes the minimum increase in total tour length.
 '''
 
-# TSP Algorithm
-def tsp():
+import sys
 
-	return 0
 
+# Farthest insertion tour construction algorithm
+def farthest_insertion (graph):
+	return graph
+
+# 2-OPT tour improvement algorithm
+def two_opt (graph):
+	return graph
+
+# TSP management function
+def tsp (graph):
+	farthest_insertion(graph)
+	two_opt(graph)
+	return graph
+
+#Converts txt file to python dict of format {node:(x,y)}
+def file_to_dict (file):
+	graph = {}
+	file = open(file,'r+')
+	for line in file:
+		(node,x,y) = line.split(" ")
+		graph[int(node)] = (x,y)
+	return graph
+
+# Validates input file
+def validate (file):
+	# Add validation code
+	return file
+
+# Main function
 def main ():
-	tsp()
+	file = validate(sys.argv[1])
+	print tsp(file_to_dict(file))
 
 
 if __name__ == '__main__':
