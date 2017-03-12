4607433028a662358887388361cb3b77690dee1a
Alacr1ty <corderda@oregonstate.edu>
2017-03-11 19:21:39 -0800

    Deleted some comments

--- tsp.py
+++ tsp.py
@@ -4,13 +4,11 @@
 # Ava Cordero
 # Alston Godbolt
 # Soo-Min Yoo
-	Nearest Insertion TSP : By Soo-Min, Ava, and Alston
-	<# I'm open to using a different alg too ~Ava >
-		Starting from a degenerate tour consisting of the two closest 
-	cities, repeatedly choose the non-tour city with the minimum 
-	distance to its nearest neighbor among the tour cities, and insert 
-	it in between the two consecutive tour cities for which such an 
-	insertion causes the minimum increase in total tour length.
+	Starting from a degenerate tour consisting of the two closest 
+cities, repeatedly choose the non-tour city with the minimum 
+distance to its nearest neighbor among the tour cities, and insert 
+it in between the two consecutive tour cities for which such an 
+insertion causes the minimum increase in total tour length.
 '''
 
 # TSP Algorithm
